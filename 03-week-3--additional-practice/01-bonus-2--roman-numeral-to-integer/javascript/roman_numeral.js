0d3d0c61b3c129147a39877e9f13ba2b48b60780
Jon Kaplon <jon.kaplon1@gmail.com>
2022-07-28 10:00:03 -0700

    roman-numeral

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,21 +1,46 @@
 function romanNumeral(string) {
   // type your code here
+
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    const current = roman[string[i]];
+    const next = roman[string[i + 1]];
+
+    if (current < next) {
+      result += next - current;
+      i++;
+    } else {
+      result += current;
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 1");
-  console.log(romanNumeral('I'));
+  console.log(romanNumeral("I"));
 
   console.log("");
 
   console.log("Expecting: 9");
-  console.log(romanNumeral('IX'));
+  console.log(romanNumeral("IX"));
 
   console.log("");
 
   console.log("Expecting: 402");
-  console.log(romanNumeral('CDII'));
+  console.log(romanNumeral("CDII"));
 }
 
 module.exports = romanNumeral;
