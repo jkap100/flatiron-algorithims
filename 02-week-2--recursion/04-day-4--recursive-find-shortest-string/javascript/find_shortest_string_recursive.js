ec4015191d024de818891835a0f93dcecf676fc2
Jon Kaplon <jon.kaplon1@gmail.com>
2022-07-28 09:21:44 -0700

    find-the-shortest-string-recursive

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
@@ -1,21 +1,30 @@
 function findShortestStringRecursive(arr) {
   // type your code here
+
+  if (arr.length === 1) return arr[0];
+
+  let result = findShortestStringRecursive(arr.slice(1));
+
+  return arr[0].length <= result.length ? arr[0] : result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestStringRecursive(["aaa", "a", "bb", "ccc"]));
 
   console.log("");
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestStringRecursive(['cat', 'hi', 'dog', 'an']));
+  console.log("=>", findShortestStringRecursive(["cat", "hi", "dog", "an"]));
 
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+  console.log(
+    "=>",
+    findShortestStringRecursive(["flower", "juniper", "lily", "dandelion"])
+  );
 }
 
 module.exports = findShortestStringRecursive;
