6abb208c5b1481acc349482d66b944b79d56e225
Jon Kaplon <jon.kaplon1@gmail.com>
2022-07-28 08:24:26 -0700

    selection-sort

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,11 +1,14 @@
 function selectionSort(arr) {
-  // type your code here
+  return arr.sort((a, b) => a - b);
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
+
+  console.log("Expecting: [-5, -1, 2, 3, 5, 10]");
+  console.log("=>", selectionSort([3, -1, 5, 2, 10, -5]));
 
   console.log("");
 
